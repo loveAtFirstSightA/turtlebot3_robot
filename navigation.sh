2cc806fa3dfc90c448e06b8020fb31201d4f8a72
lio <lioSgr1205@gmail.com>
2024-05-23 09:45:24 +0800

    sync code

---
+++ navigation.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+ros2 launch nav2_bringup navigation_launch.py use_sim_time:=true params_file:=/home/lio/turtlebot3_robot/install/nav2_bringup/share/nav2_bringup/params/nav2_params.yaml
+
---
+++ simulation.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
+
