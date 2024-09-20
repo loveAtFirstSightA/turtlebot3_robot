92ef6143a3e697fee58aeaa1ab08786b64297754
lio <lioSgr1205@gmail.com>
2024-09-20 09:34:17 +0800

    backup

---
+++ localization_factory.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+ros2 launch nav2_bringup localization_launch.py map:=/home/lio/project_repository/robot_system/maps/factory.yaml
+
--- nav.sh
+++ nav.sh
@@ -3,3 +3,5 @@
 source install/local_setup.bash
 
 ros2 launch nav2_bringup bringup_launch.py map:=/home/lio/Project_repository/turtlebot3_robot/src/navigation2/nav2_bringup/maps/stage1.yaml use_sim_time:=true
+
+
